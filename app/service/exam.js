41f9fdb768d9f501051e4193f95683e491aac806
LuckyAbby <luckyabby97@gmail.com>
2018-04-30 15:30:30 +0800

    update

--- app/service/exam.js
+++ app/service/exam.js
@@ -3,12 +3,12 @@
 const TABLE = 'exam';
 
 module.exports = app => {
-  class User extends app.Service {
+  class Exam extends app.Service {
     constructor(ctx) {
       super(ctx);
       this.table = TABLE;
     }
   }
 
-  return User;
+  return Exam;
 };
