73c0f936b2698998ff8725d604fed0a8f90dd28b
LuckyAbby <luckyabby97@gmail.com>
2018-05-06 11:08:23 +0800

    update

--- app/controller/examinee.js
+++ app/controller/examinee.js
@@ -7,11 +7,10 @@ class ExamineeController extends Controller {
   async index() {
     const { ctx, service } = this;
     const { exam_id } = ctx.query;
-    console.log('exam_id', exam_id);
     const examinees = await service.examinee.query(
       'select ue.id, user_id, name, sex, tel, email, exam_id from user_exam ue, user where user.id = ue.user_id and exam_id = ?'
       , [ exam_id ]);
-    console.log('examinees', examinees);
+    // console.log('examinees', examinees);
     ctx.body = {
       success: true,
       content: {
